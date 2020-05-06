ad1eb136274a4504eab24bc1ab731ebc8d2ddedf
Joshua Mayers <jshuam0@gmail.com>
2020-05-06 23:30:37 +1000

    Finished transformations
    Signed-off-by: Joshua Mayers <jshuam0@gmail.com>

---
+++ transformations/main.cpp
@@ -0,0 +1,230 @@
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <algorithm>
+#include <functional>
+#include <iostream>
+
+#include "shader.h"
+
+float vertices[] = {
+    // positions          // colors           // texture coords
+    0.5f,  0.5f,  0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, // top right
+    0.5f,  -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, // bottom right
+    -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom left
+    -0.5f, 0.5f,  0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f  // top left
+};
+
+unsigned int indices[] = {
+    0, 1, 3, // first triangle
+    1, 2, 3  // second triangle
+};
+
+constexpr unsigned int numModels = 3;
+constexpr unsigned int numShaders = 2;
+constexpr unsigned int numTextures = 2;
+
+unsigned int VAOs[numModels];
+unsigned int VBOs[numModels];
+unsigned int textures[numTextures];
+
+Shader shaderPrograms[numShaders];
+
+std::function<void()> drawFunc;
+
+void setupVAOs();
+void createTexture(const char* filename, unsigned int texture, GLint format, GLint sourceFormat);
+void drawTexturedContainer();
+void drawContainerColored();
+void draw2Textures();
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, true);
+    }
+    if (glfwGetKey(window, GLFW_KEY_1) == GLFW_PRESS)
+    {
+        drawFunc = drawTexturedContainer;
+    }
+    if (glfwGetKey(window, GLFW_KEY_2) == GLFW_PRESS)
+    {
+        drawFunc = drawContainerColored;
+    }
+    if (glfwGetKey(window, GLFW_KEY_3) == GLFW_PRESS)
+    {
+        drawFunc = draw2Textures;
+    }
+}
+
+int main(void)
+{
+    if (!glfwInit())
+    {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Hello Window", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (glewInit() != GLEW_OK)
+    {
+        std::cerr << "Failed to initialize GLEW" << std::endl;
+        return -1;
+    }
+
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetKeyCallback(window, key_callback);
+
+    glClearColor(0.529f, 0.808f, 0.980f, 1.0f);
+
+    setupVAOs();
+
+    glGenTextures(numTextures, textures);
+    createTexture("container.jpg", textures[0], GL_RGB, GL_RGB);
+    createTexture("awesomeface.png", textures[1], GL_RGBA, GL_RGBA);
+
+    shaderPrograms[0].loadShaders("texture.vs", "texture.fs");
+    shaderPrograms[1].loadShaders("texture.vs", "textureColor.fs");
+
+    shaderPrograms[0].use();
+    shaderPrograms[0].setInt("texture1", 0);
+    shaderPrograms[0].setInt("texture2", 1);
+
+    drawFunc = drawTexturedContainer;
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        drawFunc();
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glDeleteVertexArrays(3, VAOs);
+    glDeleteBuffers(1, VBOs);
+    glfwTerminate();
+
+    return 0;
+}
+
+void drawTexturedContainer()
+{
+    glBindTexture(GL_TEXTURE_2D, textures[0]);
+    shaderPrograms[0].use();
+
+    glm::mat4 trans = glm::mat4(1.0f);
+    trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f));
+    trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0, 0.0, 1.0));
+
+    unsigned int transformLoc = glGetUniformLocation(shaderPrograms[0].id, "transform");
+    glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
+
+    glBindVertexArray(VAOs[0]);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+}
+
+void drawContainerColored()
+{
+    glBindTexture(GL_TEXTURE_2D, textures[0]);
+    shaderPrograms[1].use();
+
+    unsigned int transformLoc = glGetUniformLocation(shaderPrograms[1].id, "transform");
+    glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(glm::mat4(1.0f)));
+
+    glBindVertexArray(VAOs[0]);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+}
+
+void draw2Textures()
+{
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, textures[0]);
+
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_2D, textures[1]);
+
+    shaderPrograms[0].use();
+
+    glm::mat4 trans = glm::mat4(1.0f);
+    trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f));
+    trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0, 0.0, 1.0));
+
+    unsigned int transformLoc = glGetUniformLocation(shaderPrograms[0].id, "transform");
+    glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
+
+    glBindVertexArray(VAOs[0]);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+}
+
+void setupVAOs()
+{
+    unsigned int IBO;
+
+    glGenVertexArrays(3, VAOs);
+    glGenBuffers(1, VBOs);
+    glGenBuffers(1, &IBO);
+
+    glBindVertexArray(VAOs[0]);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBOs[0]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+}
+
+void createTexture(const char* filename, unsigned int texture, GLint format, GLint sourceFormat)
+{
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    int width, height, numChannels;
+    stbi_set_flip_vertically_on_load_thread(true);
+    unsigned char* data = stbi_load(filename, &width, &height, &numChannels, 0);
+
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, sourceFormat, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    stbi_image_free(data);
+}
